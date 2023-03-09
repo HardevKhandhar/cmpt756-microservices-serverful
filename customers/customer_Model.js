1d6b1f9fc7797250c933997235a7871f3ddc9fdf
Hardev Khandhar <hardev.khandhar09@gmail.com>
2023-03-08 21:48:08 -0800

    Initial Commit

---
+++ customers/customer_Model.js
@@ -0,0 +1,18 @@
+const mongoose =  require ('mongoose');
+
+mongoose.model("Customer", {
+    //Name, Age, Address
+    name: {
+        type: String,
+        require: true
+    },
+    age: {
+        type: String,
+        require: true
+    },
+    address: {
+        type: String,
+        require: true
+    }
+
+});
